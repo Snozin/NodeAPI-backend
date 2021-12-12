c88c3c6f7a5bec9aaeaf9e6111ac44838c4590a7
Misael Duarte <msl.snzn@gmail.com>
2021-12-12 09:09:36 +0000

    Finalizado CRUD de Adverts

--- server/routes/adverts.js
+++ server/routes/adverts.js
@@ -1,29 +1,18 @@
 import express from 'express'
-// import asyncHandler from 'express-async-handler'
 import path from 'path'
 import { Advert } from '../models'
 
 const router = express.Router()
 
-// router.get(
-//   '/',
-//   asyncHandler(async (req, res) => {
+router.get('/', async (req, res, next) => {
+  try {
+    const file = path.join(__dirname, '../../anuncios.json')
+    const numAdverts = await Advert.loadJSON(file)
 
-//     const file = path.join(__dirname, '../../anuncios.json')
-
-//     const numAdverts = await Advert.loadJSON(file)
-
-//     // res.send(`Se han insertado: ${numAdverts} anuncios`)
-//     res.json(numAdverts)
-//   })
-// )
-
-router.get('/', async (req, res) => {
-  const file = path.join(__dirname, '../../anuncios.json')
-
-  const numAdverts = await Advert.loadJSON(file)
-
-  res.send(`Se han insertado: ${numAdverts} anuncios`)
+    res.send(`Se han insertado: ${numAdverts} anuncios`)
+  } catch (error) {
+    next(error)
+  }
   // res.json(numAdverts)
 })
 
--- server/routes/api/adverts.js
+++ server/routes/api/adverts.js
@@ -1,27 +1,82 @@
-import express, { json } from 'express'
+import express from 'express'
 import createError from 'http-errors'
-// import asyncHandler from 'express-async-handler'
 import { Advert } from '../../models'
 
 const router = express.Router()
 
+const msg404 = 'No se encontró el elemento:'
+
 router.get('/', async (req, res, next) => {
   try {
     const adverts = await Advert.find()
-    res.json({ data: adverts })
+
+    res.json({ result: adverts })
   } catch (error) {
-    next(createError(500, `Error al obtener datos de la BD. ${error}`))
+    next(error)
   }
 })
 
 router.get('/:id', async (req, res, next) => {
-  const _id = req.params.id
   try {
+    const _id = req.params.id
     const advert = await Advert.find({ _id })
-    
-    res.json({ data: advert })
+
+    if (advert.length === 0) {
+      next(createError(404, `${msg404} ${_id}`))
+      // res.status(404).json({ error: `${msg404} ` })
+      return
+    }
+
+    res.json({ result: advert })
   } catch (error) {
-    next(createError(500, `Error al obtener el valor con id: ${_id}. ${error}`))
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const advertData = req.body
+    const advert = new Advert(advertData)
+    const createdAdvert = await advert.save()
+
+    res.status(201).json({ result: createdAdvert })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const _id = req.params.id
+    const { deletedCount } = await Advert.deleteOne({ _id })
+
+    if (deletedCount === 0) {
+      next(createError(404, `${msg404} ${_id}`))
+      return
+    }
+
+    res.json({ result: 'deleted.' })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const _id = req.params.id
+    const advertData = req.body
+    const advert = await Advert.findOneAndUpdate({ _id }, advertData, {
+      new: true, //Devuelve el estado después de la actualización
+    })
+
+    if (!advert) {
+      next(createError(404, `${msg404} ${_id}`))
+      return
+    }
+
+    res.json({ result: advert })
+  } catch (error) {
+    next(error)
   }
 })
 
