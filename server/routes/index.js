f1f374a07fc91d6e21396e40a2cf31dc4b1ea571
Misael Duarte <msl.snzn@gmail.com>
2021-12-12 21:46:51 +0000

    Cleaning up

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,14 +2,6 @@ import express from 'express'
 import { Advert } from '../models'
 const router = express.Router()
 
-/* GET home page. */
-// router.get('/', (req, res, next) => {
-// Formas de pasar variables a la vista. LOCAL PARA ESTA VISTA
-// res.locals.title = 'NodeAPI'
-// res.render('index', {name: paco})
-// next()
-// })
-
 router.get('/', async (req, res, next) => {
   res.locals.title = 'NodeAPI'
   try {
